465c61c7c9e87347a0c27af62138076b29e7dabf
zombkit <stephen.korzen@gmail.com>
2025-08-24 21:57:59 -0700

    j

--- src/thing.rs
+++ src/thing.rs
@@ -3,9 +3,9 @@ use crate::*;
 #[derive(Clone, Debug)]
 pub enum Thing {
     MetaLocation { name: String, meta: MetaLocation },
-    Location(Location),
+    Location { name: String, typik: LocationType },
     Person(CharacterSheet),
-    Item(Item),
+    Item { name: String, typik: ItemType },
 }
 #[derive(Clone, Debug)]
 pub enum MetaLocation {
@@ -14,27 +14,12 @@ pub enum MetaLocation {
     Street,
 }
 
-/// A location in the world graph.
-#[derive(Clone, Debug)]
-pub struct Location {
-    pub name: String,
-
-    pub kind: LocationType,
-}
-
 /// Type of location (helps categorize behavior, appearance, etc.)
 #[derive(Clone, Debug)]
 pub enum LocationType {
     Room,
     Hall,
     StreetSection,
-}
-
-/// An item in the world.
-#[derive(Clone, Debug)]
-pub struct Item {
-    pub name: String,
-    pub kind: ItemType,
 }
 
 /// Type of item
@@ -47,13 +32,20 @@ pub enum ItemType {
 }
 
 impl Thing {
-    /// Create a new location.
-    pub fn new_location(name: &str, kind: LocationType) -> Self {
-        Thing::Location(Location {
+    /// Create a new meta-location.
+    pub fn new_meta_location(name: &str, meta: MetaLocation) -> Self {
+        Thing::MetaLocation {
             name: name.to_string(),
+            meta,
+        }
+    }
 
-            kind,
-        })
+    /// Create a new location.
+    pub fn new_location(name: &str, typik: LocationType) -> Self {
+        Thing::Location {
+            name: name.to_string(),
+            typik,
+        }
     }
 
     /// Create a new person.
@@ -63,18 +55,18 @@ impl Thing {
 
     /// Create a new item.
     pub fn new_item(name: &str, kind: ItemType) -> Self {
-        Thing::Item(Item {
+        Thing::Item {
             name: name.to_string(),
-            kind,
-        })
+            typik: kind,
+        }
     }
 
     /// Get the "display name" of the thing.
     pub fn name(&self) -> &str {
         match self {
-            Thing::Location(loc) => &loc.name,
+            Thing::Location { name, .. } => name.as_str(),
             Thing::Person(person) => &person.name,
-            Thing::Item(item) => &item.name,
+            Thing::Item { name, .. } => name.as_str(),
             Thing::MetaLocation { name, .. } => name.as_str(),
         }
     }
